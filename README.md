f381558d3bb18fba53ef25a13ddf30fdbd8f1344
William Yang <williamyang2017@gmail.com>
2022-02-07 20:06:51 +1100

    Updated build targets

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Introduction
 A statically linked library for the docking branch of imgui.
 Uses the win32 backend and dx11 graphics api. 
-Compiled on x64.
+
+Build targets are:
+- host=x64 target=x64
+- host=x86 target=x86
--- imgui_docking-config.cmake
+++ imgui_docking-config.cmake
@@ -13,13 +13,24 @@ get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 # create our library
 add_library(imgui_docking STATIC IMPORTED)  
-set_target_properties(imgui_docking PROPERTIES 
-    INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
-    IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE   "CXX"
-    IMPORTED_LOCATION_RELEASE   "${_IMPORT_PREFIX}/lib/release/imgui_docking.lib"
-    IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG     "CXX"
-    IMPORTED_LOCATION_DEBUG     "${_IMPORT_PREFIX}/lib/debug/imgui_docking.lib"
-    )
+
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set_target_properties(imgui_docking PROPERTIES 
+        INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+        IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE   "CXX"
+        IMPORTED_LOCATION_RELEASE   "${_IMPORT_PREFIX}/lib/x64/Release/imgui_docking.lib"
+        IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG     "CXX"
+        IMPORTED_LOCATION_DEBUG     "${_IMPORT_PREFIX}/lib/x64/Debug/imgui_docking.lib")
+    message(STATUS "Using x64 build of imgui_docking")
+elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+    set_target_properties(imgui_docking PROPERTIES 
+        INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+        IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE   "CXX"
+        IMPORTED_LOCATION_RELEASE   "${_IMPORT_PREFIX}/lib/x86/Release/imgui_docking.lib"
+        IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG     "CXX"
+        IMPORTED_LOCATION_DEBUG     "${_IMPORT_PREFIX}/lib/x86/Debug/imgui_docking.lib")
+    message(STATUS "Using x86 build of imgui_docking")
+endif()
 
 set_property(TARGET imgui_docking APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE DEBUG)
 
